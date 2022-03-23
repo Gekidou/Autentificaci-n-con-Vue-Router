340f955a17be6d86bc05326ef1e98ead865260a7
Fabian <therealfabi@hotmail.com>
2022-03-23 17:58:32 -0300

    subiendo desafio

---
+++ src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/components/Home";
+import Login from "@/components/Login";
+import { getAuth as auth } from "firebase/auth";
+Vue.use(Router);
+const routes = [
+  {
+    path: "*",
+    redirect: "/login",
+  },
+  {
+    path: "/",
+    redirect: "/login",
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: Home,
+    meta: {
+      login: true,
+    },
+  },
+];
+
+const router = new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  let user = auth().currentUser;
+  let authRequired = to.matched.some((route) => route.meta.login);
+  if (!user && authRequired) {
+    next("login");
+  } else if (user && !authRequired) {
+    next("home");
+  } else {
+    next();
+  }
+});
+
+export default router;
